a55f94348cf042abb8c622a227f786e3598d65eb
Arsen <gutsal.arsen@softsky.com.ua>
2012-04-25 16:49:20 +0300

    for all <a href="#"/> making href empty

--- _attachments/js/custom.js
+++ _attachments/js/custom.js
@@ -13,7 +13,7 @@ $(function () {
         }
     })
 
-    $("a[href=#]").attr("href", "")
+    $("a[href$=#]").attr("href", "")
 
     $(".dropdown-toggle").dropdown();
     $("a[data-controls-modal]").click(function(){
